08385c92e4cb3ad115a296c55f6f1513871b4af6
gabo13 <49297083+gabo13@users.noreply.github.com>
2023-11-21 11:59:20 +0100

    Update crypto.py

--- src/crypto.py
+++ src/crypto.py
@@ -1,2 +1,11 @@
+#REST API ROOT ENDPOINT:
 #UAT_SANDBOX = "https://uat-api.3ona.co/exchange/v1/{method}"
 #PRODUCTION = "https://api.crypto.com/exchange/v1/{method}"
+
+#WEBSOCKET ROOT ENDPOINTS:
+#UAT_SANDBOX:
+#  Websocket (User API and Subscriptions) wss://uat-stream.3ona.co/exchange/v1/user
+#  Websocket (Market Data Subscriptions) wss://uat-stream.3ona.co/exchange/v1/market
+#PRODUCTION:
+#Websocket (User API and Subscriptions) wss://stream.crypto.com/exchange/v1/user
+Websocket (Market Data Subscriptions) wss://stream.crypto.com/exchange/v1/market
